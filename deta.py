84ade9870f2c2b9f4f20b1170e38e393a448eaf4
wlyi1 <82428894+wlyi1@users.noreply.github.com>
2022-10-12 09:20:54 +0700

    Update deta.py

--- deta.py
+++ deta.py
@@ -1,62 +1,29 @@
 import streamlit as st
 from deta import Deta
 
-DETA_KEY = st.secrets['project_key']
-deta = Deta(DETA_KEY)
+# Data to be written to Deta Base
+with st.form("form"):
+    name = st.text_input("Your name")
+    age = st.number_input("Your age")
+    submitted = st.form_submit_button("Store in database")
 
-db = deta.Base('random1')
 
-def insert_period(period, incomes, expenses, comment):
-    """Returns the report on a successful creation, otherwise raises an error"""
-    return db.put({"key": period, "incomes": incomes, "expenses": expenses, "comment": comment})
-  
-def fetch_all_periods():
-    """Returns a dict of all periods"""
-    res = db.fetch()
-    return res.items
+# Connect to Deta Base with your Project Key
+deta = Deta(st.secrets["deta_key"])
 
-def get_period(period):
-    """If not found, the function will return None"""
-    return db.get(period)
-  
-incomes = ["Salary", "Blog", "Other Income"]
-expenses = ["Rent", "Utilities", "Groceries", "Car", "Other Expenses", "Saving"]
-currency = "USD"
-page_title = "Income and Expense Tracker"
-page_icon = ":money_with_wings:"  # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
-layout = "centered"
+# Create a new database "example-db"
+# If you need a new database, just use another name.
+db = deta.Base("example-db")
 
-st.title("My App")
-st.write("It's working!")
-if st.button("Click me!"):
-  st.write("Button works!")
-  
-if selected == "Data Entry":
-    st.header(f"Data Entry in {currency}")
-    with st.form("entry_form", clear_on_submit=True):
-        col1, col2 = st.columns(2)
-        col1.selectbox("Select Month:", months, key="month")
-        col2.selectbox("Select Year:", years, key="year")
+# If the user clicked the submit button,
+# write the data from the form to the database.
+# You can store any data you want here. Just modify that dictionary below (the entries between the {}).
+if submitted:
+    db.put({"name": name, "age": age})
 
-        "---"
-        with st.expander("Income"):
-            for income in incomes:
-                st.number_input(f"{income}:", min_value=0, format="%i", step=10, key=income)
-        with st.expander("Expenses"):
-            for expense in expenses:
-                st.number_input(f"{expense}:", min_value=0, format="%i", step=10, key=expense)
-        with st.expander("Comment"):
-            comment = st.text_area("", placeholder="Enter a comment here ...")
-
-        "---"
-        submitted = st.form_submit_button("Save Data")
-        if submitted:
-            period = str(st.session_state["year"]) + "_" + str(st.session_state["month"])
-            incomes = {income: st.session_state[income] for income in incomes}
-            expenses = {expense: st.session_state[expense] for expense in expenses}
-            db.insert_period(period, incomes, expenses, comment)
-            st.success("Data saved!")
-            
-            
-period = get_all_periods()
-st.write(period)
+"---"
+"Here's everything stored in the database:"
+# This reads all items from the database and displays them to your app.
+# db_content is a list of dictionaries. You can do everything you want with it.
+db_content = db.fetch().items
+st.write(db_content)
