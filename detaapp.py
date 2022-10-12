59fe72aee48b704c61a5772bc4f96961129fd26c
wlyi1 <82428894+wlyi1@users.noreply.github.com>
2022-10-12 09:51:15 +0700

    Update detaapp.py

--- detaapp.py
+++ detaapp.py
@@ -25,8 +25,10 @@ if submitted:
 "Here's everything stored in the database:"
 # This reads all items from the database and displays them to your app.
 # db_content is a list of dictionaries. You can do everything you want with it.
-db_content = db.fetch().items
-
+db_content = db.fetch()
+dbs = db_content.items
+per = [item['key'] for item in items]
+st.write(per)
 #df = pd.DataFrame(db_content)
 #st.dataframe(df) 
 st.write(db_content)
